21203984d2eac13e1415fac02fdda43055b7e52b
Jay <jay@Jays-Air.attlocal.net>
2025-03-17 09:30:31 -0500

    Drop old backtesting module

--- strategy/models/strategies/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Strategy implementations module.
-
-This module contains concrete strategy implementations that can be used with the backtesting engine.
-"""
-
-from .beta import BetaStrategy
-
-__all__ = ["BetaStrategy"]
