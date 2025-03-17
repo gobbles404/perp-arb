a013119291f367aac11e55832b73b3b7e53a146d
Jay <jay@Jays-Air.attlocal.net>
2025-03-17 15:24:45 -0500

    Funding arb script

--- backtesting/examples/__init__.py
+++ backtesting/examples/__init__.py
@@ -3,9 +3,11 @@
 # Import example functions
 from .basic_arb import run_basic_arbitrage
 from .zscore_strategy import run_zscore_strategy
+from .funding_arbitrage import run_funding_arbitrage
 
 # Define exported modules
 __all__ = [
     "run_basic_arbitrage",
     "run_zscore_strategy",
+    "run_funding_arbitrage",
 ]
